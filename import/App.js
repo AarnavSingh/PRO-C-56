52b22eb53796fab536a676ae490e4219c1f1c06c
AarnavSingh <77780842+AarnavSingh@users.noreply.github.com>
2021-07-12 16:07:05 +0530

    Add files via upload

---
+++ import/App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import soundButton from './components/soundButton';
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <soundButton/>
+      </View>
+    );
+  }
+}
+
+
+
+
+  
